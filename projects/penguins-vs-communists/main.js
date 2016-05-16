104cbd98a27409bad9e2f5459f0ab8e977cd4046
Andre Burke <aburkex24@gmail.com>
2016-05-15 21:32:02 -0600

    updates to penguins-vs-communists

--- projects/penguins-vs-communists/main.js
+++ projects/penguins-vs-communists/main.js
@@ -1,14 +1,11 @@
-function Parties(party) {
-    this.population = 1000000000;
+function Parties(party, population) {
+    this.population = population;
     this.party = party;
 }
 
-var penguins = new Parties("Penguins");
-
-var communists = new Parties("Communists");
-
+var penguins = new Parties("Penguins", 1000000000);
+var communists = new Parties("Communists", 1000000000);
 var firstAttack = Math.floor(Math.random() * 2) + 1;
-
 var party = "";
 
 if (firstAttack === 1) {
@@ -19,30 +16,40 @@ if (firstAttack === 1) {
 
 function sendNuke(party, onHit, onMiss) {
     console.log("The " + party + " will be the first to attack.");
-    var randomAttack = Math.floor(Math.random() * 3) + 1; 
+    var randomAttack = Math.floor(Math.random() * 3) + 1;
     if (randomAttack === 3) {
-        onHit(party); 
+        onHit(party);
     } else {
-        onMiss(party); 
+        onMiss(party);
+    }
+    var randomDamage = Math.floor(Math.random() * 150000) + 100000;
+    if (party === "Penguins") {
+        penguins.population = penguins.population - randomDamage;
+        console.log("The Communists counter and attack back.");
+        console.log("The Penguins lose " + randomDamage + " of their population.");
+    } else {
+        communists.population = communists.population - randomDamage;
+        console.log("The Penguins counter and attack back");
+        console.log("The Communists lose " + randomDamage + " of their population.");
     }
 }
 
 function onHit(party) {
-    console.log("The attack was successful, the " + party + " hit them head on!"); 
-    var randomDamage = Math.floor(Math.random() * 150000) + 100000; 
-    
+    console.log("The attack was successful, the " + party + " hit them head on!");
+    var randomDamage = Math.floor(Math.random() * 150000) + 100000;
+
     if (party === "Penguins") {
-        communists.population = communists.population - randomDamage; 
-        console.log("The Communists lost " + randomDamage + " of their population."); 
-        console.log(communists.population); 
-    } else {
-        communists.population = communists.population - randomDamage; 
+        communists.population = communists.population - randomDamage;
         console.log("The Communists lost " + randomDamage + " of their population.");
+        console.log(communists.population);
+    } else {
+        penguins.population = penguins.population - randomDamage;
+        console.log("The Penguins lost " + randomDamage + " of their population.");
     }
 }
 
 function onMiss(party) {
-    console.log("The attack missed, the " + party + " are proving to be quite distasteful..."); 
+    console.log("The attack missed, the " + party + " are proving to be quite distasteful...");
 }
 
-sendNuke(party, onHit, onMiss); 
+sendNuke(party, onHit, onMiss);
