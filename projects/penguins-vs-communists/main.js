ad9f710aa4f1694cff1008409e142199839b2410
Andre Burke <aburkex24@gmail.com>
2016-05-15 18:56:20 -0600

    updates to penguins-vs-communists

--- projects/penguins-vs-communists/main.js
+++ projects/penguins-vs-communists/main.js
@@ -4,34 +4,36 @@ function Party(party) {
 }
 
 var penguins = new Party("Penguins");
-console.log(penguins); 
+//console.log(penguins); 
 
 var communists = new Party("Communists");
-console.log(communists); 
+//console.log(communists); 
 
 var attacksFirst = Math.floor(Math.random() * 2) + 1;
 
-console.log(attacksFirst); 
+//console.log(attacksFirst); 
 
 
 function sendNuke(party, onHit, onMiss) {
     if (attacksFirst === 1) {
-        console.log("The " + party + " will be attacked."); 
+        console.log("The " + party + " will be attacked.");
     } else {
-        console.log("The " + party + " will be attacked."); 
+        console.log("The " + party + " will be attacked.");
     }
-    var hitOrMiss = Math.floor(Math.random() * 3) + 1; 
+    var hitOrMiss = Math.floor(Math.random() * 3) + 1;
     //console.log(hitOrMiss); 
-    var randomDamage = Math.floor(Math.random() * 90000) + 25000; 
+    var randomDamage = Math.floor(Math.random() * 90000) + 25000;
     //console.log(randomDamage); 
 }
 
 function onHit(party) {
-    sendNuke(party); 
+    sendNuke(party);
 }
 
 function onMiss(party) {
-    sendNuke(party); 
+    sendNuke(party);
 }
 
-sendNuke(Party(), onHit, onMiss); 
+if (attacksFirst === 1) {
+    
+}
