7c14e3ffbdd4b289f51aa7005c027a74ba103370
Andre Burke <aburkex24@gmail.com>
2016-05-17 20:14:06 -0600

    moved some functions out of the game function to make them standalone

--- projects/penguins-vs-communists/main.js
+++ projects/penguins-vs-communists/main.js
@@ -18,53 +18,8 @@ function game() {
         } else {
             party = communists.party;
         }
-        
+
         sendNuke(party, onHit, onMiss); 
-
-        function sendNuke(party, onHit, onMiss) {
-            console.log("The " + party + " will be the first to attack.");
-            console.log();
-            var randomAttack = Math.floor(Math.random() * 3) + 1;
-            if (randomAttack === 3) {
-                onHit(party);
-            } else {
-                onMiss(party);
-            }
-            var randomDamage = Math.floor(Math.random() * 200000) + 100000;
-            if (party === "Penguins") {
-                penguinsHealth = penguinsHealth - randomDamage;
-                console.log("The Communists counter and attack back.");
-                console.log();
-                console.log("The Penguins lose " + randomDamage + " of their population.");
-                console.log();
-            } else {
-                communistHealth = communistHealth - randomDamage;
-                console.log("The Penguins counter and attack back");
-                console.log();
-                console.log("The Communists lose " + randomDamage + " of their population.");
-                console.log();
-            }
-        }
-
-        function onHit(party) {
-            console.log("The attack was successful, the " + party + " hit them head on!");
-            console.log();
-            var randomDamage = Math.floor(Math.random() * 125000) + 75000;
-            if (party === "Penguins") {
-                communistHealth = communistHealth - randomDamage;
-                console.log("The Communists lost " + randomDamage + " of their population.");
-                console.log();
-            } else {
-                penguinsHealth = penguinsHealth - randomDamage;
-                console.log("The Penguins lost " + randomDamage + " of their population.");
-                console.log();
-            }
-        }
-
-        function onMiss(party) {
-            console.log("The attack missed, the " + party + " are proving to be quite distasteful...");
-            console.log();
-        }
 
         console.log("===============================================================================");
 
@@ -77,8 +32,55 @@ function game() {
             console.log("The Communists have been exterminated.");
             console.log();
         }
-        
-    }, 500);
+
+    }, 600);
 }
 
 game();
+
+
+
+function sendNuke(party, onHit, onMiss) {
+    console.log("The " + party + " will be the first to attack.");
+    console.log();
+    var randomAttack = Math.floor(Math.random() * 3) + 1;
+    if (randomAttack === 3) {
+        onHit(party);
+    } else {
+        onMiss(party);
+    }
+    var randomDamage = Math.floor(Math.random() * 200000) + 100000;
+    if (party === "Penguins") {
+        penguinsHealth = penguinsHealth - randomDamage;
+        console.log("The Communists counter and attack back.");
+        console.log();
+        console.log("The Penguins lose " + randomDamage + " of their population.");
+        console.log();
+    } else {
+        communistHealth = communistHealth - randomDamage;
+        console.log("The Penguins counter and attack back");
+        console.log();
+        console.log("The Communists lose " + randomDamage + " of their population.");
+        console.log();
+    }
+}
+
+function onHit(party) {
+    console.log("The attack was successful, the " + party + " hit them head on!");
+    console.log();
+    var randomDamage = Math.floor(Math.random() * 125000) + 75000;
+    if (party === "Penguins") {
+        communistHealth = communistHealth - randomDamage;
+        console.log("The Communists lost " + randomDamage + " of their population.");
+        console.log();
+    } else {
+        penguinsHealth = penguinsHealth - randomDamage;
+        console.log("The Penguins lost " + randomDamage + " of their population.");
+        console.log();
+    }
+}
+
+function onMiss(party) {
+    console.log("The attack missed, the " + party + " are proving to be quite distasteful...");
+    console.log();
+}
